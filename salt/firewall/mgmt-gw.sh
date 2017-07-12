87d042e1020120119f8465fc0040e0f7b48c21b0
Astro <astro@spaceboyz.net>
2017-07-13 00:23:59 +0200

    firewall.mgmt-gw: permit radius.hq.c3d2.de

--- salt/firewall/mgmt-gw.sh
+++ salt/firewall/mgmt-gw.sh
@@ -19,6 +19,8 @@ ip6tables -A FORWARD -i $IFACE -p udp --dport 25826 -j ACCEPT
 # downloads.lede-project.org
 iptables -A FORWARD -i $IFACE --dest 148.251.78.235 -j ACCEPT
 ip6tables -A FORWARD -i $IFACE --dest 2a01:4f8:202:43ea::3 -j ACCEPT
+# radius.hq.c3d2.de
+iptables -A FORWARD -i $IFACE --dest 172.22.99.22 -j ACCEPT
 # Deny by default
 iptables -A FORWARD -j REJECT
 ip6tables -A FORWARD -j REJECT
