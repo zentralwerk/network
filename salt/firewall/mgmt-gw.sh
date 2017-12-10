d9d6c8cff004d913c1dd16aa824caa4b2592b070
Astro <astro@spaceboyz.net>
2017-12-11 00:36:10 +0100

    Revert "apply mgmt-gw's firewall via lxc-hook"
    This reverts commit 2f202d7b2f0d8d56a0152ec0c671c00e8d59eb9f.
    
    The referenced mgmt-gw.sh gets provisioned inside the container so it
    does not make sense to call it on the host.

--- salt/firewall/mgmt-gw.sh
+++ salt/firewall/mgmt-gw.sh
@@ -2,27 +2,28 @@
 
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
-IFACE=mgmt
-iptables -F FORWARD
-ip6tables -F FORWARD
-iptables -P FORWARD DROP
-ip6tables -P FORWARD DROP
-iptables -A FORWARD -m state --state ESTABLISHED -j ACCEPT
-ip6tables -A FORWARD -m state --state ESTABLISHED -j ACCEPT
-# DNS
-iptables -A FORWARD -i $IFACE -p udp --dport 53 -j ACCEPT
-ip6tables -A FORWARD -i $IFACE -p udp --dport 53 -j ACCEPT
-# NTP
-iptables -A FORWARD -i $IFACE -p udp --dport 123 -j ACCEPT
-ip6tables -A FORWARD -i $IFACE -p udp --dport 123 -j ACCEPT
-# collectd
-iptables -A FORWARD -i $IFACE -p udp --dport 25826 -j ACCEPT
-ip6tables -A FORWARD -i $IFACE -p udp --dport 25826 -j ACCEPT
-# downloads.lede-project.org
-iptables -A FORWARD -i $IFACE --dest 148.251.78.235 -j ACCEPT
-ip6tables -A FORWARD -i $IFACE --dest 2a01:4f8:202:43ea::3 -j ACCEPT
-# radius.hq.c3d2.de
-iptables -A FORWARD -i $IFACE --dest 172.22.99.22 -j ACCEPT
-# Deny by default
-iptables -A FORWARD -j REJECT
-ip6tables -A FORWARD -j REJECT
+if [ "$IFACE" = "{{ interface }}" ]; then
+    iptables -F FORWARD
+    ip6tables -F FORWARD
+    iptables -P FORWARD DROP
+    ip6tables -P FORWARD DROP
+    iptables -A FORWARD -m state --state ESTABLISHED -j ACCEPT
+    ip6tables -A FORWARD -m state --state ESTABLISHED -j ACCEPT
+    # DNS
+    iptables -A FORWARD -i $IFACE -p udp --dport 53 -j ACCEPT
+    ip6tables -A FORWARD -i $IFACE -p udp --dport 53 -j ACCEPT
+    # NTP
+    iptables -A FORWARD -i $IFACE -p udp --dport 123 -j ACCEPT
+    ip6tables -A FORWARD -i $IFACE -p udp --dport 123 -j ACCEPT
+    # collectd
+    iptables -A FORWARD -i $IFACE -p udp --dport 25826 -j ACCEPT
+    ip6tables -A FORWARD -i $IFACE -p udp --dport 25826 -j ACCEPT
+    # downloads.lede-project.org
+    iptables -A FORWARD -i $IFACE --dest 148.251.78.235 -j ACCEPT
+    ip6tables -A FORWARD -i $IFACE --dest 2a01:4f8:202:43ea::3 -j ACCEPT
+    # radius.hq.c3d2.de
+    iptables -A FORWARD -i $IFACE --dest 172.22.99.22 -j ACCEPT
+    # Deny by default
+    iptables -A FORWARD -j REJECT
+    ip6tables -A FORWARD -j REJECT
+fi
